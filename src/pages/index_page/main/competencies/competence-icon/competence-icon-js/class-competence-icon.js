8ec9f859547bad346494669261a1965bc6fb2c71
arturprogwork <arturprogwork@gmail.com>
2024-08-30 21:47:06 +0300

    start to creating class for icon-object

---
+++ src/pages/index_page/main/competencies/competence-icon/competence-icon-js/class-competence-icon.js
@@ -0,0 +1,11 @@
+class competenceIcon {
+    constructor () {
+        this.tagli = document.createElement("li");
+    }
+
+}
+
+
+// соддать <li>
+// вставить в id="competencies-list"
+// проверить в доме
